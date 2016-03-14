210d0530d0b89ecaf964771a72670a126119e5be
haukerehme <hauke.rehme-schlueter@hotmail.com>
2016-03-14 21:30:53 +0100

    Add testifLastEurUsdIsNull

--- src/Test/TestGetGbpjpy.java
+++ src/Test/TestGetGbpjpy.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package boerse;
+package Test;
 
+import boerse.GetAllLiveClosewerte;
 import java.net.MalformedURLException;
 
 /**
